0a3d8b7353ab60a409ec1ab47ed3faae3e85df8a
Danny Ben Shitrit <db@dannyben.com>
2020-01-31 09:05:56 +0000

    make integration tests mandatory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,12 +14,12 @@ require_relative 'spec_mixin'
 RSpec.configure do |c|
   c.include SpecMixin
 
-  have_codenamize = command_exist? 'codenamize'
+  # have_codenamize = command_exist? 'codenamize'
 
-  unless have_codenamize
-    c.filter_run_excluding :codenamize
-    c.after :suite do 
-      puts "\n\u001b[31mNote: Compatibility tests were skipped since codenamize is not installed\u001b[0m"
-    end
-  end
+  # unless have_codenamize
+  #   c.filter_run_excluding :codenamize
+  #   c.after :suite do 
+  #     puts "\n\u001b[31mNote: Compatibility tests were skipped since codenamize is not installed\u001b[0m"
+  #   end
+  # end
 end
